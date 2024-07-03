a9941d53450ed2865b653b6ab42ec8bc0bb70e99
FernandaGabrielli <fernandagabriellidev@gmail.com>
2024-07-02 22:47:49 -0300

    fix: trying to fix bug

--- mysite/tests/models/test_post.py
+++ mysite/tests/models/test_post.py
@@ -1,8 +1,6 @@
 
 import pytest
-from blog.factories import PostFactory
-
-
+from mysite.blog.factories import PostFactory
 @pytest.fixture
 def post_published():
     return PostFactory(title='pytest with factory')
