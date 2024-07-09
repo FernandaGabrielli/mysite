3f331936417b99c1e3e92654ee8e28cebb3b5bdc
FernandaGabrielli <fernandagabriellidev@gmail.com>
2024-07-08 21:44:32 -0300

    Templates

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -6,8 +6,8 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "status", "created_on")
-    list_filter =  ("status",)
-    search_fields = ["tite", "content"]
+    list_filter = ("status",)
+    search_fields = ["title", "content"]
     prepopulated_fields = {'slug': ('title',)}
 
 
